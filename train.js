d3fdb4c4fb7099f800b50981a1ebd87e2d2927f0
TJ Abbing <tjabbing@gmail.com>
2023-06-26 18:50:54 +0200

    Added; function to prepare and train machine learning model on patient data.

--- train.js
+++ train.js
@@ -8,7 +8,25 @@ function loadData(){
             complete: results => checkData(results.data)
         })
     } loadData()
-    
-    function checkData(data) {
+
+    function checkData(data){
         console.table(data)
+
+        // data voorbereiden
+        data.sort(() => (Math.random() - 0.5))
+        let trainData = data.slice(0, Math.floor(data.length * 0.8))
+        let testData = data.slice(Math.floor(data.length * 0.8) + 1)
+    
+        // neural network aanmaken
+        let nn = ml5.neuralNetwork({ task: 'regression', debug: true })
+    
+        // data toevoegen aan neural network
+        for(let patient of trainData){
+            nn.addData({ age: patient.age, sex: patient.sex, bmi: patient.bmi, children: patient.children }, {charges: patient.charges })
+        }
+
+        // normalize data
+        nn.normalizeData(console.log(`Normalized data.`));
+
+        nn.train({ epochs: 10 }, () => console.log("Finished training."));
     }
