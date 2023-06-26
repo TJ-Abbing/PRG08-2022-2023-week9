1ce7c26676dff4749e3f50206fcf04f9a2308ae6
TJ Abbing <tjabbing@gmail.com>
2023-06-26 15:38:54 +0200

    Added; load CSV function.

--- train.js
+++ train.js
@@ -1,14 +1,14 @@
 import { createChart, updateChart } from "./scatterplot.js"
 
-//
-// demo data
-//
-const data = [
-        { horsepower: 130, mpg: 18 },
-        { horsepower: 165, mpg: 15 },
-        { horsepower: 225, mpg: 14 },
-        { horsepower: 97, mpg: 18 },
-        { horsepower: 88, mpg: 27 },
-        { horsepower: 193, mpg: 9 },
-        { horsepower: 80, mpg: 25 },
-]
+function loadData(){
+        Papa.parse("./data/insurance.csv", {
+            download:true,
+            header:true, 
+            dynamicTyping:true,
+            complete: results => checkData(results.data)
+        })
+    } loadData()
+    
+    function checkData(data) {
+        console.table(data)
+    }
