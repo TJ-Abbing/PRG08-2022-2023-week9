14510e9e4f1d2e0bfd9721638d745da1fb72b432
TJ Abbing <tjabbing@gmail.com>
2023-06-27 09:09:51 +0200

    Added; logs. Fixed; logs.

--- train.js
+++ train.js
@@ -1,6 +1,9 @@
+console.log(`Loaded train.js.`)
 import { createChart, updateChart } from "./scatterplot.js"
 
 function loadData(){
+
+    console.log(`Initialized loadData().`)
         Papa.parse("./data/insurance.csv", {
             download:true,
             header:true, 
@@ -9,38 +12,42 @@ function loadData(){
         })
     } loadData()
 
-    function checkData(data){
-        console.table(data)
+function checkData(data){
 
-        // data voorbereiden
-        data.sort(() => (Math.random() - 0.5))
-        let trainData = data.slice(0, Math.floor(data.length * 0.8))
-        let testData = data.slice(Math.floor(data.length * 0.8) + 1)
-    
-        // neural network aanmaken
-        let nn = ml5.neuralNetwork({ task: 'regression', debug: true })
-    
-        // data toevoegen aan neural network
-        for(let patient of trainData){
-            nn.addData({ age: patient.age, sex: patient.sex, bmi: patient.bmi, children: patient.children }, {charges: patient.charges })
-        }
+    console.log(`Initialized checkData().`)
+    console.table(data)
 
-        // normalize data
-        nn.normalizeData(console.log(`Normalized data.`));
+    // data voorbereiden
+    data.sort(() => (Math.random() - 0.5))
+    let trainData = data.slice(0, Math.floor(data.length * 0.8))
+    let testData = data.slice(Math.floor(data.length * 0.8) + 1)
 
-        nn.train({ epochs: 10 }, () => console.log("Finished training."));
+    // neural network aanmaken
+    let nn = ml5.neuralNetwork({ task: 'regression', debug: true })
 
-        async function makePrediction(){
-
-                for (let patient of testData) {
-                        const testPatient = { age: patient.age, sex: patient.sex, bmi: patient.bmi, children: patient.children };
-                        const pred = nn.predict(testPatient);
-                        console.log(`Predicted price for phone with the following details; 
-                        \n cores: ${patient.age}
-                        \n cpu: ${patient.sex}
-                        \n memory: ${patient.bmi}
-                        \n storage: ${patient.children}
-                        \n price: ${pred[0].charges}`);
-                }
-        }makePrediction();
+    // data toevoegen aan neural network
+    for(let patient of trainData){
+        nn.addData({ age: patient.age, sex: patient.sex, bmi: patient.bmi, children: patient.children }, {charges: patient.charges })
     }
+
+    // normalize data
+    nn.normalizeData(console.log(`Normalized data.`));
+
+    nn.train({ epochs: 10 }, () => console.log("Finished training."));
+
+    async function makePrediction(){
+        
+        console.log(`Initialized makePrediction().`)
+
+        for (let patient of testData) {
+                const testPatient = { age: patient.age, sex: patient.sex, bmi: patient.bmi, children: patient.children };
+                const pred = nn.predict(testPatient);
+                console.log(`Predicted charge for patient with the following details; 
+                \n age: ${patient.age}
+                \n sex: ${patient.sex}
+                \n bmi: ${patient.bmi}
+                \n children: ${patient.children}
+                \n charges: ${pred[0].charges}`);
+        }
+    }makePrediction();
+}
